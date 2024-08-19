3b94bd025ae1f2d451a1a9cb0d2f896ec50a2f32
Varun Balani <varunprahladb@gmail.com>
2024-08-20 03:20:04 +0530

    add google layer

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,29 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+          integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+          crossOrigin=""
+        />
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/@geoman-io/leaflet-geoman-free@latest/dist/leaflet-geoman.css"
+        />
+
+        <script
+          src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+          integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+          crossOrigin=""
+          defer
+        ></script>
+        <script
+          src="https://unpkg.com/@geoman-io/leaflet-geoman-free@latest/dist/leaflet-geoman.js"
+          defer
+        ></script>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
